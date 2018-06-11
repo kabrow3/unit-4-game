196cac625f9fca3785766e597f540e35a168b3ec
kabrow3 <kabrow3@gmail.com>
2018-06-11 16:27:04 -0400

    3rd commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # unit-4-game
+
+This is the Crystal Game Project.
+
+It was completed for the fourth Homework Assignment for the Coding Boot Camp.
+
+It is based in javascript and jQuery.
+
+It generates a random goal number as well as four crystal buttons with random numbers assigned to each of them. The object is to reach the goal number by clicking on the various crystal buttons and adding amounts to your score.
+
+The game keeps track and displays the goal number, the current score of the user, the instructions for the game, and number of wins and losses so far, the title of the game, and the crystal buttons themselves.
+
+After a game is won or lost, the win/loss record is updated, the goal number is reset and the values of each crystal button are reset as well.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,49 +5,69 @@
   var crystalNum2 = Math.floor((Math.random()*12) + 1);
   var crystalNum3 = Math.floor((Math.random()*12) + 1);
   var crystalNum4 = Math.floor((Math.random()*12) + 1);
+  var numberOptions = [crystalNum1, crystalNum2, crystalNum3, crystalNum4];
+  var imageOptions = ["assets/images/crystal1.jpg","assets/images/crystal2.jpg","assets/images/crystal3.jpg","assets/images/crystal4.jpg"];
+  var imageCrystal1;
+  var imageCrystal2;
+  var imageCrystal3;
+  var imageCrystal4;
+  var imageCrystals = [imageCrystal1, imageCrystal2, imageCrystal3, imageCrystal4];
 
   var userScore = 0;
   var wins = 0;
   var losses = 0;
-
-
-
-
-
 
   $("#target").text(targetNumber);
   $("#wins").text("Wins " + wins);
   $("#losses").text("Losses " + losses);
   $("#score").text(userScore);
 
+
+  function reset() {
+    userScore = 0;
+    targetNumber = Math.floor((Math.random()*101) + 19);
+    crystalNum1 = Math.floor((Math.random()*12) + 1);
+    crystalNum2 = Math.floor((Math.random()*12) + 1);
+    crystalNum3 = Math.floor((Math.random()*12) + 1);
+    crystalNum4 = Math.floor((Math.random()*12) + 1);
+    numberOptions = [crystalNum1, crystalNum2, crystalNum3, crystalNum4];
+    for (var i = 0; i < 4; i++) {
+      imageCrystals[i].attr("data-crystalvalue", numberOptions[i]);
+    }
+  
+    $("#target").text(targetNumber);
+    $("#score").text(userScore);
+  }
+
   var crystals = $("#crystals");
 
   // Now for the hard part. Creating multiple crystals each with their own unique number value.
 
   // We begin by expanding our array to include four options.
-  var numberOptions = [crystalNum1, crystalNum2, crystalNum3, crystalNum4];
+  
+  
 
   // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
+  for (var i = 0; i < 4; i++) {
 
     // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
+    imageCrystals[i] = $("<img>");
 
     // First each crystal will be given the class ".crystal-image".
     // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
+    imageCrystals[i].addClass("crystal-image");
 
     // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystals[i].attr("src", imageOptions[i]);
 
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    imageCrystals[i].attr("data-crystalvalue", numberOptions[i]);
 
-    console.log(imageCrystal.attr("data-crystalvalue"));
+    console.log(imageCrystals[i].attr("data-crystalvalue"));
 
     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    crystals.append(imageCrystal);
+    crystals.append(imageCrystals[i]);
   }
 
   // This time, our click event applies to every single crystal on the page. Not just one.
@@ -72,12 +92,14 @@
       alert("You win!");
       wins++;
       $("#wins").text("Wins " + wins);
+      reset();
     }
 
     else if (userScore >= targetNumber) {
       alert("You lose!!");
       losses++;
       $("#losses").text("Losses " + losses);
+      reset();
     }
 
   });
